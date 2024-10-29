6fc1b68ee006cb0a38956143350d341358e00113
Romulo <romuloass@hotmail.com>
2024-10-29 09:43:44 -0300

    ajustes para webcriver

--- ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
+++ ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
@@ -3,60 +3,70 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver import ChromeOptions
+from time import sleep
+import unittest
 import pathlib
 
-# Prepara o WebDriver
-driver = webdriver.Chrome()
-file_path = pathlib.Path(__file__).parent.resolve()
-driver.get(f"file:///{file_path}/sample-exercise.html")
+class TestStringMethods(unittest.TestCase):
 
-# Teste Simples pra verificar se a pagina foi aberta corretamente
-title = driver.title
-assert title == "Sample page"
-time.sleep(1) 
+    @classmethod
+    def setUpClass(cls):
+        # Prepara o WebDriver
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        cls.driver = webdriver.Chrome(options=options)
+        file_path = pathlib.Path(__file__).parent.resolve()
+        cls.driver.get(f"file:///{file_path}/sample-exercise_.html")
 
-try:
-    # Clica no botão generate
-    generate_button = driver.find_element(By.NAME, "generate")
-    generate_button.click()
+    def test_page_load(self):
+        # Teste Simples pra verificar se a pagina foi aberta corretamente
+        title = self.driver.title
+        self.assertEqual(title, "Sample page")
 
-    # Aguarda o código ser gerado
-    WebDriverWait(driver, 5).until(
-        lambda d: d.find_element(By.ID, "my-value").text != ""
-    )
+    def test_generate_and_check_alert(self):
+        try:
+            # Clica no botão generate
+            generate_button = self.driver.find_element(By.NAME, "generate")
+            generate_button.click()
 
-    # Pega o Codigo gerado
-    generated_code = driver.find_element(By.ID, "my-value").text
-    
-    time.sleep(1) 
+            # Aguarda o código ser gerado
+            WebDriverWait(self.driver, 5).until(
+                lambda d: d.find_element(By.ID, "my-value").text != ""
+            )
 
-    # Limpa o texto de texto e coloca o codigo gerado
-    input_field = driver.find_element(By.ID, "input")
-    input_field.clear()
-    input_field.send_keys(generated_code)
+            # Pega o Codigo gerado
+            generated_code = self.driver.find_element(By.ID, "my-value").text
+            
+            # Limpa o texto de texto e coloca o codigo gerado
+            input_field = self.driver.find_element(By.ID, "input")
+            input_field.clear()
+            input_field.send_keys(generated_code)
 
-    # Clica no botão Test
-    test_button = driver.find_element(By.NAME, "button")
-    test_button.click()
+            # Clica no botão Test
+            test_button = self.driver.find_element(By.NAME, "button")
+            test_button.click()
 
-    time.sleep(1) 
+            # Aguarda o alerta aparecer, verificar se está ok e fecha
+            WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+            alert = Alert(self.driver)
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Done!")
+            alert.accept()
 
-    # Aguarda o alerta aparecer, verificar se está ok e fecha
-    WebDriverWait(driver, 5).until(EC.alert_is_present())
-    alert = Alert(driver)
-    alert_text = alert.text
-    assert alert_text == "Done!", f"Unexpected alert text: {alert_text}"
-    time.sleep(1) 
-    alert.accept()
+            # Verifica se a mensagem está com o código gerado
+            result_message = self.driver.find_element(By.ID, "result").text
+            expected_message = f"It works! {generated_code}!"
+            self.assertEqual(result_message, expected_message)
+        
+        finally:
+            sleep(2)
 
-    # Verifica se a mensagem está com o código gerado
-    result_message = driver.find_element(By.ID, "result").text
-    expected_message = f"It workls! {generated_code}!"
-    time.sleep(1) 
-    assert result_message == expected_message, f"Unexpected result message: {result_message}"
+    @classmethod
+    def tearDownClass(cls):
+        # Fecha o Browser
+        cls.driver.quit()
 
-finally:
-    # Fecha o Browser
-    time.sleep(2)
-    driver.quit()
+if __name__ == "__main__":
+    unittest.main()
