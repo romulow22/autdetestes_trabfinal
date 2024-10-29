4b3f208bff2e1f210ea76641a7b729d8504af2e8
Romulo <romuloass@hotmail.com>
2024-10-29 09:50:29 -0300

    revisao

--- ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
+++ ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ChromeOptions
-from time import sleep
 import unittest
 import pathlib
+import os
 
-class TestStringMethods(unittest.TestCase):
+class TestGenerateAndCheckAlert(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -17,51 +17,56 @@ class TestStringMethods(unittest.TestCase):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         cls.driver = webdriver.Chrome(options=options)
-        file_path = pathlib.Path(__file__).parent.resolve()
-        cls.driver.get(f"file:///{file_path}/sample-exercise_.html")
-
-    def test_page_load(self):
-        # Teste Simples pra verificar se a pagina foi aberta corretamente
-        title = self.driver.title
-        self.assertEqual(title, "Sample page")
+        cls.rootdir = pathlib.Path(__file__).parent.resolve()
+        cls.driver.get(f"file:///{cls.rootdir}/sample-exercise_.html")
+        print(f"File Path: {cls.rootdir}")
+        print(f"Script Name: {os.path.basename(__file__)}")
 
     def test_generate_and_check_alert(self):
-        try:
-            # Clica no botão generate
-            generate_button = self.driver.find_element(By.NAME, "generate")
-            generate_button.click()
+        self.click_generate_button()
+        generated_code = self.wait_for_and_get_generated_code()
+        self.enter_generated_code(generated_code)
+        self.click_test_button()
+        self.check_alert()
+        self.verify_result_message(generated_code)
 
-            # Aguarda o código ser gerado
-            WebDriverWait(self.driver, 5).until(
-                lambda d: d.find_element(By.ID, "my-value").text != ""
-            )
+    def click_generate_button(self):
+        # Clica no botão generate
+        generate_button = self.driver.find_element(By.NAME, "generate")
+        generate_button.click()
 
-            # Pega o Codigo gerado
-            generated_code = self.driver.find_element(By.ID, "my-value").text
-            
-            # Limpa o texto de texto e coloca o codigo gerado
-            input_field = self.driver.find_element(By.ID, "input")
-            input_field.clear()
-            input_field.send_keys(generated_code)
+    def wait_for_and_get_generated_code(self):
+        # Aguarda o código ser gerado
+        WebDriverWait(self.driver, 5).until(
+            lambda d: d.find_element(By.ID, "my-value").text != ""
+        )
+        # Pega o Codigo gerado
+        return self.driver.find_element(By.ID, "my-value").text
 
-            # Clica no botão Test
-            test_button = self.driver.find_element(By.NAME, "button")
-            test_button.click()
+    def enter_generated_code(self, code):
+        # Limpa o texto de texto e coloca o codigo gerado
+        input_field = self.driver.find_element(By.ID, "input")
+        input_field.clear()
+        input_field.send_keys(code)
 
-            # Aguarda o alerta aparecer, verificar se está ok e fecha
-            WebDriverWait(self.driver, 5).until(EC.alert_is_present())
-            alert = Alert(self.driver)
-            alert_text = alert.text
-            self.assertEqual(alert_text, "Done!")
-            alert.accept()
+    def click_test_button(self):
+        # Clica no botão Test
+        test_button = self.driver.find_element(By.NAME, "button")
+        test_button.click()
 
-            # Verifica se a mensagem está com o código gerado
-            result_message = self.driver.find_element(By.ID, "result").text
-            expected_message = f"It workls! {generated_code}!"
-            self.assertEqual(result_message, expected_message)
-        
-        finally:
-            sleep(2)
+    def check_alert(self):
+        # Aguarda o alerta aparecer, verificar se está ok e fecha
+        WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        alert = Alert(self.driver)
+        alert_text = alert.text
+        self.assertEqual(alert_text, "Done!")
+        alert.accept()
+
+    def verify_result_message(self, code):
+        # Verifica se a mensagem está com o código gerado
+        result_message = self.driver.find_element(By.ID, "result").text
+        expected_message = f"It works! {code}!"
+        self.assertEqual(result_message, expected_message)
 
     @classmethod
     def tearDownClass(cls):
