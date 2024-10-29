cc0f48455e94c641056c1d6f7496332433e0523b
Romulo <romuloass@hotmail.com>
2024-10-29 10:05:56 -0300

    simplificacao

--- ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
+++ ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ChromeOptions
-from time import sleep
 import unittest
 import pathlib
 import os
@@ -26,45 +25,34 @@ class BaseTest(unittest.TestCase):
     def tearDownClass(cls):
         cls.driver.quit()
 
-class TestPageLoad(BaseTest):
-
-    def test_page_load(self):
-        title = self.driver.title
-        self.assertEqual(title, "Sample page")
-
 class TestGenerateAlert(BaseTest):
 
     def test_generate_and_check_alert(self):
-        try:
-            generate_button = self.driver.find_element(By.NAME, "generate")
-            generate_button.click()
+        generate_button = self.driver.find_element(By.NAME, "generate")
+        generate_button.click()
 
-            WebDriverWait(self.driver, 5).until(
-                lambda d: d.find_element(By.ID, "my-value").text != ""
-            )
+        WebDriverWait(self.driver, 5).until(
+            lambda d: d.find_element(By.ID, "my-value").text != ""
+        )
 
-            generated_code = self.driver.find_element(By.ID, "my-value").text
-            
-            input_field = self.driver.find_element(By.ID, "input")
-            input_field.clear()
-            input_field.send_keys(generated_code)
-
-            test_button = self.driver.find_element(By.NAME, "button")
-            test_button.click()
-
-            WebDriverWait(self.driver, 5).until(EC.alert_is_present())
-            alert = Alert(self.driver)
-            alert_text = alert.text
-            self.assertEqual(alert_text, "Done!")
-            alert.accept()
-
-            result_message = self.driver.find_element(By.ID, "result").text
-            expected_message = f"It workls! {generated_code}!"
-            self.assertEqual(result_message, expected_message)
+        generated_code = self.driver.find_element(By.ID, "my-value").text
         
-        finally:
-            sleep(2)
+        input_field = self.driver.find_element(By.ID, "input")
+        input_field.clear()
+        input_field.send_keys(generated_code)
+
+        test_button = self.driver.find_element(By.NAME, "button")
+        test_button.click()
+
+        WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        alert = Alert(self.driver)
+        alert_text = alert.text
+        self.assertEqual(alert_text, "Done!")
+        alert.accept()
+
+        result_message = self.driver.find_element(By.ID, "result").text
+        expected_message = f"It workls! {generated_code}!"
+        self.assertEqual(result_message, expected_message)
 
 if __name__ == "__main__":
     unittest.main()
-
