427fe71f9b562aed193839672dfa0d78e967c4b4
Romulo <romuloass@hotmail.com>
2024-10-29 10:02:47 -0300

    revisao

--- ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
+++ ferramentas/02_selenium_exercise/pratica-automacao-de-testes.py
@@ -9,11 +9,10 @@ import unittest
 import pathlib
 import os
 
-class TestGenerateAndCheckAlert(unittest.TestCase):
+class BaseTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Prepara o WebDriver
         options = ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
@@ -23,43 +22,42 @@ class TestGenerateAndCheckAlert(unittest.TestCase):
         print(f"File Path: {cls.rootdir}")
         print(f"Script Name: {os.path.basename(__file__)}")
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+class TestPageLoad(BaseTest):
+
     def test_page_load(self):
-        # Teste Simples pra verificar se a pagina foi aberta corretamente
         title = self.driver.title
         self.assertEqual(title, "Sample page")
 
+class TestGenerateAlert(BaseTest):
 
     def test_generate_and_check_alert(self):
         try:
-            # Clica no botão generate
             generate_button = self.driver.find_element(By.NAME, "generate")
             generate_button.click()
 
-            # Aguarda o código ser gerado
             WebDriverWait(self.driver, 5).until(
                 lambda d: d.find_element(By.ID, "my-value").text != ""
             )
 
-            # Pega o Codigo gerado
             generated_code = self.driver.find_element(By.ID, "my-value").text
             
-            # Limpa o texto de texto e coloca o codigo gerado
             input_field = self.driver.find_element(By.ID, "input")
             input_field.clear()
             input_field.send_keys(generated_code)
 
-            # Clica no botão Test
             test_button = self.driver.find_element(By.NAME, "button")
             test_button.click()
 
-            # Aguarda o alerta aparecer, verificar se está ok e fecha
             WebDriverWait(self.driver, 5).until(EC.alert_is_present())
             alert = Alert(self.driver)
             alert_text = alert.text
             self.assertEqual(alert_text, "Done!")
             alert.accept()
 
-            # Verifica se a mensagem está com o código gerado
             result_message = self.driver.find_element(By.ID, "result").text
             expected_message = f"It workls! {generated_code}!"
             self.assertEqual(result_message, expected_message)
@@ -67,10 +65,6 @@ class TestGenerateAndCheckAlert(unittest.TestCase):
         finally:
             sleep(2)
 
-    @classmethod
-    def tearDownClass(cls):
-        # Fecha o Browser
-        cls.driver.quit()
-
 if __name__ == "__main__":
     unittest.main()
+
